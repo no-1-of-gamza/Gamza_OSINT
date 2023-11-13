2db61ce4d632207e4d478d0c6a985950e6efc6a8
( DO DREAM DEVELOPMENT) DEVELOPER D 실행하고 꿈꾸고 발전하는개발자.. 가 되고싶습니다 <68416184+silver-liq9118@users.noreply.github.com>
2023-11-13 17:04:13 +0900

    Update OSINT_API.py

--- OSINT_API.py
+++ OSINT_API.py
@@ -3,28 +3,11 @@ import os
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'groom-gamza-osint-7d145849be1a.json'
 
-# Imports the Google Cloud client library
 from google.cloud import vision
 
-# Instantiates a client
 client = vision.ImageAnnotatorClient()
 
-# The name of the image file to annotate
 file_name = os.path.abspath('example.jpg')
-
-# Loads the image into memory
-with io.open(file_name, 'rb') as image_file:
-    content = image_file.read()
-
-image = vision.Image(content=content)
-
-# Performs label detection on the image file
-response = client.label_detection(image=image)
-labels = response.label_annotations
-
-print('Labels:')
-for label in labels:
-    print(label.description)
 
 
 def detect_text(path):
