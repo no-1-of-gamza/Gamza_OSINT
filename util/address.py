d606921592d182c1fc152ee58cfee11ff4d39c84
yura-Choi <dbfk1207@gmail.com>
2023-11-15 15:49:45 +0900

    Modify path to open address file

--- util/address.py
+++ util/address.py
@@ -1,9 +1,10 @@
 import json
+import os
 
 
 class Address:
 	def __init__(self):
-		with open("address.txt", "r", encoding="utf-8-sig") as file:
+		with open(os.path.dirname(os.path.realpath(__file__))+"\\address.txt", "r", encoding="utf-8-sig") as file:
 			data = file.read()
 			self.address_book = json.loads(data)
 
