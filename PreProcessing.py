a200dcafa6ff52bd346f1a1ea9ca0e7c10125c42
ryues0 <141699180+ryues0@users.noreply.github.com>
2023-11-15 17:08:03 +0900

    Update PreProcessing.py

--- PreProcessing.py
+++ PreProcessing.py
@@ -5,13 +5,15 @@ dictionary_list = [
     {'title': '주소지', 'url': 'https:~', 'preview': '미리보기', 'content': '전체 내용'}
 ]
 '''
-from util import Address
+from util import crawler
+from util import address
 from util import NaverMapApi
 
-class Crawling:
+class Data_PreProcessing:
     def __init__(self):
-        self.check = Address()
-        self.api = NaverMapApi()
+        self.crawler = crawler.Crawler()
+        self.check = address.Address()
+        self.api = NaverMapApi.NaverMapApi()
         self.api_word = []
     
     # 주소 인근 단어 추출
@@ -23,31 +25,30 @@ class Crawling:
         # 주소 값일 경우, on/off 둘다 추가, 하지만 off일 때 주소 값이 왔다면 on으로 변경(시작임을 알림)
         # '경기도 광주 삼겹살 식당' -> '경기도 광주 삼겹살'까지 저장
 
-        for item in data_list:
-            if self.check.get(data_list[i]) == -1:
-                if separate == 'on':
-                    temp += item
-                    result.append(temp)
-                    temp = ''
-                    separate = 'off'
+        for item_list in data_list:
+            for item in item_list:
+                if self.check.get(item) == -1:
+                    if separate == 'on':
+                        temp += item
+                        result.append(temp)
+                        temp = ''
+                        separate = 'off'
+                    else:
+                        pass
                 else:
-                    pass
-            else:
-                if separate == 'on':
-                    temp += item
-                else: 
-                    temp += item
-                    separate = 'on'
-
+                    if separate == 'on':
+                        temp += item
+                    else: 
+                        temp += item
+                        separate = 'on'
         return result
-
         
     def Pre_Processing(self, crawling_list):
         # 특수 문자 제거
         special_charavters = '!@#$%^&*()_+<>?:"{}|[];\',.'
 
         for item in crawling_list:
-            for key, value in item.itmes():
+            for key, value in item.items():
                 for char in special_charavters:
                     value = value.replace(char, ' ')
                 item[key] = value
@@ -58,11 +59,11 @@ class Crawling:
         content = []
         for item in crawling_list:
             if 'title' in item:
-                title.append(crawling_list['title'].split())
+                title.append(item['title'].split())
             elif 'preview' in item:
-                preview.append(crawling_list['preview'].split())
+                preview.append(item['preview'].split())
             elif 'content' in item:
-                content.append(crawling_list['content'].split())
+                content.append(item['content'].split())
             else:
                 pass
         
@@ -71,9 +72,6 @@ class Crawling:
         self.api_word.extend(self.for_api(preview))
         self.api_word.extend(self.for_api(content))
 
-        # 중복 제거
-        self.api_word = list(set(self.api_word))
-
         # naver api
         address_dic = {}
         roadAddress_dic = {}
@@ -81,3 +79,18 @@ class Crawling:
             address, roadAddress = self.api.get(item)
             address_dic[item] = address
             roadAddress_dic[item] = roadAddress
+        
+        return address_dic, roadAddress_dic
+
+    def start(self):
+        image_text = "성북구 동선동 2가 에그맛있다"
+        crawling_list = self.crawler.start(image_text)
+        result_add, result_road = self.Pre_Processing(crawling_list)
+        return result_add, result_road
+
+if __name__ == "__main__":
+    data = Data_PreProcessing()
+    result_add, result_road = data.start()
+    print("address: \n", result_add)
+    print("\n")
+    print("road address: \n", result_road)
